171291e23bf167b59196b507cdae0f81e58d4869
Mariga123 <johnmariga8@gmail.com>
2020-11-29 20:51:53 +0300

    initialize blueprint

--- app/main/views.py
+++ app/main/views.py
@@ -27,6 +27,6 @@ def articles(id):
 	view articles page
 	'''
 	articles = get_articles(id)
-	title = f'{id}'
+	title = f'NH | {id}'
 
 	return render_template('articles.html',title= title,articles = articles)
---
+++ start.sh
@@ -0,0 +1,4 @@
+export API_KEY= '7691462879d443a6bbe19862df24c634'
+
+
+python3.7 manage.py server
