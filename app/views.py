065d26eab8f95842125d8289ea60f069a3b9d677
Mariga123 <johnmariga8@gmail.com>
2020-11-29 13:31:32 +0300

    create a class for news sources

--- app/views.py
+++ app/views.py
@@ -1,12 +1,24 @@
 from flask import render_template
 from app import app
+from .request import get_sources
+
+
 
 # Views
 @app.route('/')
 def index():
+	'''
+	view root page function that returns the index the page and its data
+	'''
 
-    '''
-    View root page function that returns the index page and its data
-    '''
-    title = 'WELCOME TO YOUR LATEST NEWS UPDATE CHANNEL'
-    return render_template('index.html', title = title)
+	general_sources = get_sources('general')
+	science_sources = get_sources('science')
+	business_sources = get_sources('business')
+	technology_sources = get_sources('technology')
+	health_sources = get_sources('health')
+	entertainment_sources = get_sources('entertainment')
+	sports_sources = get_sources('sports')
+	title = "News Highlighter"
+
+	return render_template('index.html',title = title, general_sources = general_sources, science_sources = science_sources, business_sources = business_sources,technology_sources = technology_sources, health_sources = health_sources, entertainment_sources = entertainment_sources, sports_sources = sports_sources)
+
--- news_test.py
+++ news_test.py
@@ -1,8 +1,9 @@
 import unittest
 from models import news
-News = news.News
 
-class NewsTest(unittest.TestCase):
+Sources = source.Sources
+
+class SourceTest(unittest.TestCase):
     '''
     Test Class to test the behaviour of the News class
     '''
@@ -11,10 +12,10 @@ class NewsTest(unittest.TestCase):
         '''
         Setting up method that will run before every Test
         '''
-        self.new_news = News(Mariga,'Kenyans debts still flactuating','A thrilling drift in kenyan economy','https://techcrunch.com/wp-content/uploads/2020/11/GettyImages-887657568.jpg?w=600',8.5,2000)
+        self.new_source = Source(Mariga,'Kenyans debts still flactuating','A thrilling drift in kenyan economy','https://techcrunch.com/wp-content/uploads/2020/11/GettyImages-887657568.jpg?w=600',8.5,2000)
 
     def test_instance(self):
-        self.assertTrue(isinstance(self.new_news,News))
+        self.assertTrue(isinstance(self.new_source,Source))
 
 
 if __name__ == '__main__':
